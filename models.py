c936cd4f5b19b36c8b41840d748a9dac160413a1
olratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2021-11-09 19:25:52 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
-import os
-import sys
-sys.path.append("/home/yuliamarkiv/pp")
 
 from sqlalchemy import (
    Table,
@@ -19,37 +16,40 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-DB_URI = os.getenv('DB_URI','postgresql://tecmint:rolny34F@localhost:5432/test')
+engine = create_engine("mysql://root:A2452756b@127.0.0.1/music_player")
 
+SessionFactory = sessionmaker(bind=engine)
 
-engine = create_engine(DB_URI)
-SessionFactory = sessionmaker(bind= engine)
 Session = scoped_session(SessionFactory)
+
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(VARCHAR)
-    password = Column(VARCHAR)
-    email = Column(VARCHAR)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(VARCHAR(length=200))
+    password = Column(VARCHAR(length=200))
+    email = Column(VARCHAR(length=200))
     playlists = relationship("Playlist", backref= "user")
-
 
     def __repr__(self):
         return f"<User {self.username}, email: {self.email}>"
+
 
 playlist_song = Table('playlist_song', Base.metadata,
     Column('playlist_id', ForeignKey('playlist.id'), primary_key=True),
     Column('song_id', ForeignKey('song.id'), primary_key=True)
 )
+
+
 class Song(Base):
     __tablename__ = 'song'
-    id = Column(Integer, primary_key=True)
-    name = Column(VARCHAR, nullable=False)
-    singer = Column(VARCHAR)
-    album = Column(VARCHAR)
-    duration = Column(VARCHAR,nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(VARCHAR(length=200), nullable=False)
+    singer = Column(VARCHAR(length=200))
+    album = Column(VARCHAR(length=200))
+    duration = Column(VARCHAR(length=200),nullable=False)
 
     def __repr__(self):
         return f"<Song {self.name} {self.singer} {self.album}  {self.duration}>"
@@ -57,13 +57,11 @@ class Song(Base):
 
 class Playlist(Base):
     __tablename__ = "playlist"
-    id = Column(Integer,primary_key=True)
-    title = Column(VARCHAR)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(VARCHAR(length=200))
     songs = relationship(Song, secondary= playlist_song, lazy="subquery", backref=backref("playlists", lazy=True))
     user_id = Column(Integer, ForeignKey("user.id"))
     private = Column(Boolean)
+
     def __repr__(self):
         return f"<Playlist {self.title}  {self.private} , User: {self.user}>"
-
-
-
