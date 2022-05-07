8afef6fcadefc65560c50359b745fd21d39aae5d
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-05-07 15:22:11 +0300

    solve cors problem

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import timedelta
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ JWT_SECRET_KEY = 'super-secret'
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
 jwt = JWTManager(app)
+cors = CORS(app)
 
 ADMIN_NAME = 'admin'
